dc3ac3866914bed1305a2ad5b2764b1a1c1597fc
Robert Taylor <robtaylor3012@gmail.com>
2018-05-02 22:15:05 -0500

    Added changes to footer/navbar

--- angular-src/src/app/components/about/about.component.ts
+++ angular-src/src/app/components/about/about.component.ts
@@ -10,6 +10,18 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.add('presentation-page');
+    body.classList.add('loading');
+    var navbar = document.getElementsByTagName('nav')[0];
+    navbar.classList.add('navbar-transparent');
+  }
+    ngOnDestroy(){
+      var body = document.getElementsByTagName('body')[0];
+      body.classList.remove('settings');
+      var navbar = document.getElementsByTagName('nav')[0];
+      navbar.classList.remove('navbar-transparent');
+      navbar.classList.remove('bg-danger');
   }
 
 }
